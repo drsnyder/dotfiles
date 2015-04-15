f873f2df2cb23fd5f1489594e1012774c18cb290
Damon Snyder <drsnyder@gmail.com>
2015-04-15 11:40:57 -0700

    Clean up & expandtabs in vim

--- bin/wikia-tmux-init.sh
+++ bin/wikia-tmux-init.sh
@@ -4,12 +4,16 @@ tmux new-session -d -s wikia
 tmux new-window -twikia:0 -n git
 tmux new-window -twikia:1 -n dev-box
 tmux new-window -twikia:2 -n vim
-tmux new-window -twikia:3 -n empty
+tmux new-window -twikia:3 -n guidelines
+tmux new-window -twikia:4 -n vignette
+tmux new-window -twikia:5 -n Wikia-app
+tmux new-window -twikia:6 -n Pandora
+tmux new-window -twikia:7 -n SICP
 
-tmux send-keys -twikia:0 'cd Wikia; clear' C-m
-tmux send-keys -twikia:1 'cd Wikia; clear' C-m
-tmux send-keys -twikia:2 'cd Wikia; clear' C-m
-tmux send-keys -twikia:3 'cd Wikia; clear' C-m
+for pane in {0..7}; do
+	tmux send-keys -twikia:$pane 'cd Wikia; clear' C-m
+done
+
 
 tmux select-window -twikia:0
 tmux attach-session -d -twikia
