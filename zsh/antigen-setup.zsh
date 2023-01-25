a3fb2e5b2225f79ef61cf6fd22e9d441564f6147
drsnyder <github@damon.snyder.to>
2023-01-25 09:11:37 -0600

    Add antigen

---
+++ zsh/antigen-setup.zsh
@@ -0,0 +1,22 @@
+source /usr/local/share/antigen/antigen.zsh
+
+# Load the oh-my-zsh's library.
+antigen use oh-my-zsh
+
+# Bundles from the default repo (robbyrussell's oh-my-zsh).
+antigen bundle git
+antigen bundle pip
+antigen bundle lein
+antigen bundle command-not-found
+
+# Syntax highlighting bundle.
+antigen bundle zsh-users/zsh-syntax-highlighting
+
+# Mine
+antigen bundle jeffreytse/zsh-vi-mode
+
+# Load the theme.
+antigen theme clean
+
+# Tell Antigen that you're done.
+antigen apply
