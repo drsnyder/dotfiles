1c3c4b198a8c61f4ce2319f80e8f884459b0474e
Damon Snyder <damon@rebelliondefense.com>
2024-09-12 13:34:17 -0500

    Checkpoint

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,7 +1,14 @@
 alias reload!='. ~/.zshrc'
 
 alias cls='clear' # Good 'ol Clear Screen command
-
 alias notes='vim `fzf --preview="cat {}" --preview-window=right:70%:wrap`'
-
 alias ag='ag --hidden'
+alias mux=tmuxinator
+
+alias ls='lsd'
+alias l='ls -l'
+alias la='ls -a'
+alias lla='ls -la'
+alias lt='ls --tree'
+alias lrt='ls -alrth'
+
